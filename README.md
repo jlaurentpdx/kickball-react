95f4a4768ce91ba7cceff78a852c5ee6d112687d
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-03 17:03:39 -0800

    initialized client functions to retrieve Supabase data

--- README.md
+++ README.md
@@ -7,10 +7,10 @@
    `PlayerDetail.js`
    `TeamList.js`
    `TeamDetail.js`
+1. Create Navigation bar outside the scope of our Routes
 1. Set up our Routes in `App.js`
 1. Fetch data from Supabase in `services/`:
    `players.js`
    `teams.js`
-1. Create Navigation bar outside the scope of our Routes
 1. Create home page as our initial Route
 1. Initialize one of our views!
--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
+import Teams from './views/Teams';
+import Players from './views/Players';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <NavLink to="/teams">Teams</NavLink>
+          <NavLink to="/players">Players</NavLink>
+        </header>
+        <Switch>
+          <Route exact path="/teams" component={Teams} />
+          <Route exact path="/players" component={Players} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
